dc6264eb13af6892101c169a6470b1b07967c1f3
iblessme <vldikk12@gmail.com>
2022-06-28 17:00:14 +0300

    Одна вьюшка

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="home">
+  <div class="count">
+    {{storeCounter.count}}
+  </div>
+  <div class="buttons">
+    <button @click="storeCounter.decreaseCount">-</button>
+    <button @click="storeCounter.increaseCount">+</button>
+  </div>
+  <hr>
+  <div>
+    Это {{storeCounter.oddOrEven}} число
+  </div>
+  <hr>
+  <div>
+    <h3>Edit counter: </h3>
+    <input type="number" v-model="storeCounter.count">
+  </div>
+
+</div>
+
+</template>
+
+
+<script setup>
+
+import {useCounterStore} from '@/stores/counter'
+
+const storeCounter = useCounterStore()
+
+
+</script>
+
+
+<style>
+.home{
+  text-align: center;
+}
+.count{
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button{
+  font-size: 40px;
+  margin: 5px;
+}
+</style>
